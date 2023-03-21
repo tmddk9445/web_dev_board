1aeaf8e886a282eb3fa0b34fa849d3b23c4b9796
tmddk9445 <tmddk9445@naver.com>
2023-03-21 16:20:34 +0900

    20230321 TUE web_dev_board

---
+++ board/src/main/java/com/jihoon/board/service/MainService.java
@@ -0,0 +1,27 @@
+package com.jihoon.board.service;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MainService {
+
+  // //? 스케쥴 작업이 끝나는 시간 기준으로 실행
+  // @Scheduled(fixedDelay = 1000)
+  // public void scheduleFixedDelay() {
+  //   System.out.println("고정 딜레이 작업 : " + System.currentTimeMillis() / 1000);
+  // }
+
+  // //? 스케쥴 작업이 시작하는 시간 기준으로 실행
+  // @Scheduled(fixedRate = 1000) 
+  // public void scheduleFixedRate() {
+  //   System.out.println("시작 고정 딜레이 작업 : " + System.currentTimeMillis() / 1000);
+  // }
+
+  @Scheduled(cron = "2 * * * * ?")
+  public void scheduleCronJob() {
+    System.out.println("Cron Job으로 시간 지정 작업 : " + System.currentTimeMillis() / 1000);
+  }  
+
+}
+
