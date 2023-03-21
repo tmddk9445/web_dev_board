195d85540783330458db5ec3513d386cd0fe3c4c
tmddk9445 <tmddk9445@naver.com>
2023-03-21 17:13:53 +0900

    20230321 TUE wev_dev_board

--- board/src/main/java/com/jihoon/board/common/util/SampleJob.java
+++ board/src/main/java/com/jihoon/board/common/util/SampleJob.java
@@ -1,13 +1,11 @@
 package com.jihoon.board.common.util;
 
-import org.quartz.CronExpression;
 import org.quartz.CronScheduleBuilder;
 import org.quartz.Job;
 import org.quartz.JobBuilder;
 import org.quartz.JobDetail;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
-import org.quartz.ScheduleBuilder;
 import org.quartz.Trigger;
 import org.quartz.TriggerBuilder;
 import org.springframework.context.annotation.Bean;
@@ -15,32 +13,33 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class SampleJob implements Job {
-  
-  @Override
-  public void execute(JobExecutionContext context) throws JobExecutionException {
-    System.out.println("SampleJob execute!!");
-  }
 
-  @Bean
-  public JobDetail jobDetail() {
-    return JobBuilder
-    .newJob()
-    .ofType(SampleJob.class)
-    .storeDurably()
-    .withIdentity("Test Sample Job Detail")
-    .withDescription("Sample Job Detail 테스트입니다.")
-    .build();
-  }
+    @Override
+    public void execute(JobExecutionContext context) throws JobExecutionException {
+        System.out.println("SampleJob excute!!");
+    }
 
-  @Bean
-  public Trigger trigger(JobDetail jobDetail) {
-    CronScheduleBuilder cronSchedule = CronScheduleBuilder.cronSchedule("2 * * * * ?");
-    return TriggerBuilder
-    .newTrigger()
-    .forJob(jobDetail)
-    .withIdentity("Test Sample Trigger")
-    .withDescription("Sample Job Trigger 테스트입니다.")
-    .withSchedule(cronSchedule)
-    .build();
-  }
+    @Bean
+    public JobDetail jobDetail() {
+        return JobBuilder
+                .newJob()
+                .ofType(SampleJob.class)
+                .storeDurably()
+                .withIdentity("Test Sample Job Detail")
+                .withDescription("Sample Job Detail 테스트입니다.")
+                .build();
+    }
+
+    @Bean
+    public Trigger trigger(JobDetail jobDetail) {
+
+        CronScheduleBuilder cronScedule = CronScheduleBuilder.cronSchedule("2 * * * * ?");
+        
+        return TriggerBuilder.newTrigger().forJob(jobDetail)
+            .withIdentity("Test Sample Trigger")
+            .withDescription("Sample Trggier 테스트입니다.")
+            .withSchedule(cronScedule)
+            .build();
+    }
+
 }
--- board/src/main/java/com/jihoon/board/service/MainService.java
+++ board/src/main/java/com/jihoon/board/service/MainService.java
@@ -1,27 +1,47 @@
 package com.jihoon.board.service;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MainService {
+    
+    //? 스케쥴 작업이 끝나는 시간 기준으로 실행
+    // @Scheduled(fixedDelay=1000)
+    // public void scheduleFixedDelay() {
+    //     System.out.println("고정 딜레이 작업 : " + System.currentTimeMillis() / 1000);
+    // }
 
-  // //? 스케쥴 작업이 끝나는 시간 기준으로 실행
-  // @Scheduled(fixedDelay = 1000)
-  // public void scheduleFixedDelay() {
-  //   System.out.println("고정 딜레이 작업 : " + System.currentTimeMillis() / 1000);
-  // }
+    //? 스케쥴 작업이 시작하는 시간 기준으로 실행
+    // @Scheduled(fixedRate=1000)
+    // public void scheduleFixedRate() {
+    //     System.out.println("시작 고정 딜레이 작업 : " + System.currentTimeMillis() / 1000);
+    // }
 
-  // //? 스케쥴 작업이 시작하는 시간 기준으로 실행
-  // @Scheduled(fixedRate = 1000) 
-  // public void scheduleFixedRate() {
-  //   System.out.println("시작 고정 딜레이 작업 : " + System.currentTimeMillis() / 1000);
-  // }
+    @Scheduled(cron="2 * * * * ?")
+    public void scheduleCronJob() {
+        try {
+            crawlling();
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
 
-  @Scheduled(cron = "2 * * * * ?")
-  public void scheduleCronJob() {
-    System.out.println("Cron Job으로 시간 지정 작업 : " + System.currentTimeMillis() / 1000);
-  }  
+    public void crawlling() throws Exception {
+
+        Document document = Jsoup.connect("https://naver.com").get();
+
+        Elements elements = document.select("#NM_FAVORITE > div.group_nav > ul.list_nav.NM_FAVORITE_LIST > li > a");
+        System.out.println(elements.size());
+
+        for (Element element: elements) {
+            System.out.println(element.absUrl("href"));
+        }
+
+    }
 
 }
-
