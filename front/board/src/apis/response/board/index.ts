34e237ded10e1656d8f9d1a4756fc3567d00a128
tmddk9445 <tmddk9445@naver.com>
2023-03-24 17:17:02 +0900

    20230324 FRI web_dev_board Front interface setting

---
+++ front/board/src/apis/response/board/index.ts
@@ -0,0 +1,18 @@
+import DeleteBoardResponseDto from './Delete-Board.response.dto';
+import GetBoardResponseDto from './Get-Board.response.dto';
+import GetListResponseDto from './Get-List.response.dto';
+import GetMyListResponseDto from './Get-My-List.response.dto';
+import GetSearchListResponseDto from './Get-Search-List.response.dto';
+import GetTop3ListResponseDto from './Get-Top3-List.response.dto';
+import GetTop15RelatedSearchWordResponseDto from './Get-Top15-Related-Search-Word.response.dto';
+import GetTop15SearchWordResponseDto from './Get-Top15-Search-Word.response.dto';
+import LikeResponseDto from './Like.reponse.dto';
+import PatchBoardResponseDto from './Patch-Board.response.dto';
+import PostBoardResponseDto from './Post-Board.response.dto';
+import PostCommentResponseDto from './Post-Comment.response.dto';
+
+export type { DeleteBoardResponseDto, 
+  GetBoardResponseDto, GetListResponseDto, GetMyListResponseDto, GetSearchListResponseDto,
+  GetTop3ListResponseDto, GetTop15RelatedSearchWordResponseDto, GetTop15SearchWordResponseDto,
+  LikeResponseDto, PatchBoardResponseDto, PostBoardResponseDto, PostCommentResponseDto
+};
